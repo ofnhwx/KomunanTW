bd00f5ea4b407f7e4f66615654dc74bdc34dae6d
Yuta Fujita <ofnhwx@komunan.net>
2018-11-04 13:10:35 +0900

    [WIP] タイムラインの追加を実装

--- app/src/main/java/net/komunan/komunantw/ui/main/timelines/TimelinesFragment.kt
+++ app/src/main/java/net/komunan/komunantw/ui/main/timelines/TimelinesFragment.kt
@@ -1,13 +1,13 @@
 package net.komunan.komunantw.ui.main.timelines
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.simple_recycler_view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.komunan.komunantw.R
 import net.komunan.komunantw.observeOnNotNull
 import net.komunan.komunantw.ui.common.TWBaseFragment
@@ -18,16 +18,42 @@ class TimelinesFragment: TWBaseFragment() {
         fun create() = TimelinesFragment()
     }
 
+    private val viewModel by lazy { ViewModelProviders.of(this).get(TimelinesViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.simple_recycler_view, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val viewModel = ViewModelProviders.of(this).get(TimelinesViewModel::class.java)
         viewModel.timelines.observeOnNotNull(this) { timelines ->
             container.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
             container.adapter = TimelinesAdapter(timelines)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        if (menu == null) {
+            return
+        }
+        menu.add(0, R.string.add_timeline, 1, R.string.add_timeline)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.string.add_timeline -> {
+                GlobalScope.launch {
+                    viewModel.addTimeline()
+                }
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
         }
     }
 }
--- app/src/main/java/net/komunan/komunantw/ui/main/timelines/TimelinesViewModel.kt
+++ app/src/main/java/net/komunan/komunantw/ui/main/timelines/TimelinesViewModel.kt
@@ -1,10 +1,16 @@
 package net.komunan.komunantw.ui.main.timelines
 
 import androidx.lifecycle.LiveData
+import net.komunan.komunantw.R
 import net.komunan.komunantw.repository.entity.Timeline
+import net.komunan.komunantw.string
 import net.komunan.komunantw.ui.common.TWBaseViewModel
 
 class TimelinesViewModel: TWBaseViewModel() {
     val timelines: LiveData<List<Timeline>>
         get() = Timeline.findAllAsync()
+
+    fun addTimeline() {
+        Timeline(R.string.new_timeline.string()).save()
+    }
 }
